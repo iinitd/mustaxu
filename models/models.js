29f6baf0dc2d2e64ed1fc958fc0a387f094ee415
cogons <cogons@qq.com>
2016-12-06 15:04:18 +0800

    实现发帖、修改、列表

--- models/models.js
+++ models/models.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var ArticleSchema = mongoose.Schema;
 // Define User schema 
-var _User = new Schema({
-	email: String,
-	name: String
+var _Article = new ArticleSchema({
+	title: String,
+	content: String
+}, {
+	versionKey: false
 });
 
-_User.statics = {
+_Article.statics = {
 	fetch: function(cb) {
 		return this
 			.find()
 			.exec(cb)
 	},
-	findById: function(cb) {
-		return this
-			.findOne({
-				_name: name
-			})
-			.exec(cb)
+	findById: function(id, cb) {
+		return this.findOne({
+			_id: id
+		}).exec(cb);
+
 	}
 };
 // export them 
-exports.User = mongoose.model('User', _User);
+exports.Article = mongoose.model('Article', _Article);
--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,104 @@ var express = require("express");
 var mongoose = require('mongoose');
 var models = require('../models/models');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 
 mongoose.connect('mongodb://localhost/blog2');
 
-var User = models.User;
+var Article = models.Article;
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded());
 
-router.use(bodyParser.urlencoded({
-	extended: true
-}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', {
-		title: 'Express'
-	});
+
+	Article.fetch(function(err, article) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('index', {
+			article: article
+		})
+	})
+
 });
+
+/* GET home page. */
+router.get('/list', function(req, res, next) {
+
+	Article.fetch(function(err, article) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('list', {
+			article: article
+		})
+	})
+
+});
+
+router.get('/p/:id', function(req, res) {
+
+	var id = req.params.id;
+	Article.findById(id, function(err, article) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('detail', {
+			article: article
+		})
+	})
+
+});
+
+router.get('/edit/:id', function(req, res) {
+
+	var id = req.params.id;
+	Article.findById(id, function(err, article) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('edit', {
+			article: article
+		})
+	})
+
+});
+
+router.post('/edit/:id', function(req, res) {
+	var id = req.params.id;
+	var title = req.body['article[title]'];
+	var content = req.body['article[content]']
+	Article.findById(id, function(err, article2) {
+		if (err) {
+			console.log(err)
+		}
+
+		Article.update({
+			_id: id
+		}, {
+			title: title,
+			content: content
+		}, function(err, docs) { //更新
+			console.log(docs);
+			console.log('update success');
+
+		})
+
+		res.redirect('/p/' + id);
+
+	})
+
+})
+
 
 router.get('/test', function(req, res) {
 	res.render('index', {
 		title: 'index',
-		users: [{
+		articles: [{
 			title: 'hh',
 			_id: 3,
 			poster: 'h'
@@ -37,88 +113,42 @@ router.get('/admin', function(req, res) {
 
 
 
-	User.fetch(function(err, user) {
+	Article.fetch(function(err, article) {
 		if (err) {
 			console.log(err)
 		}
 		res.render('admin', {
-			user: user
+			article: article
 		})
 	})
 })
 
 router.post('/admin', function(req, res) {
-	var user = req.body.user;
-	var userObj = new User({
-		email: req.body['user[email]'],
-		name: req.body['user[name]']
+	var article = req.body.article;
+	var articleObj = new Article({
+		title: req.body['article[title]'],
+		content: req.body['article[content]']
 	});
-	userObj.save(function(err, userObj) {
+	articleObj.save(function(err, articleObj) {
 		if (err) {
 			console.log(err)
 		}
-		res.redirect('/admin')
+		res.redirect('/list')
 	});
 
 })
-
-//admin post user
-router.post('/admin', function(req, res) {
-	var id = req.body.user._id;
-	var userObj = req.body.user;
-	var _user;
-	if (id !== undefined) {
-		User.findById(id, function(err, user) {
-			if (err) {
-				console.log(err);
-			}
-			_user = _.extend(user, userObj);
-			_user.save(function(err, user) {
-				if (err) {
-					console.log(err);
-				}
-				res.redirect('/admin/');
-			});
-		});
-	} else {
-		_user = new User({
-			email: req.body['user[email]'],
-			name: req.body['user[name]']
-		});
-		_user.save(function(err, user) {
-			if (err) {
-				console.log(err);
-			}
-			res.redirect('/admin/');
-		});
-	}
-});
 
 
 router.get('/name', function(req, res) {
 
-	User.fetch(function(err, user) {
+	User.findById(function(err, user) {
 		if (err) {
 			console.log(err)
 		}
-		res.send(user)
+		res.send(article)
 	})
 
 })
-
-router.get('/init', function(req, res) {
-	var user = new User({
-		email: 'nowind_lee @qq.com',
-		name: 'Freewind'
-	});
-	user.save();
-	res.send(user.name);
-});
-
-
-router.get('/user', function(req, res) {
-	res.send('users');
-});
 
 
 
